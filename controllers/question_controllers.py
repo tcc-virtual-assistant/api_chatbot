9f86b018b882d40df0f4aaa85144055e2eccad6a
nychavs <chavesnaiury@gmail.com>
2023-02-28 13:00:51 -0300

    get, post and delete already done

--- controllers/question_controllers.py
+++ controllers/question_controllers.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from models.question import Question
+import ormar
 
 router = APIRouter()
 
@@ -11,3 +12,21 @@ async def add_question(question: Question):
 @router.get("/")
 async def list_question():
     return await Question.objects.all()
+
+@router.get('/{question_id}')
+async def questions_id(question_id: int, response: Response):
+    try: 
+        question = await Question.objects.get(id = question_id)
+        return question
+    except ormar.exceptions.NoMatch:
+        response.status_code = 404
+        return {'mensagem':'id nao encontrado'}
+    
+@router.delete('/{question_id}')
+async def delete_question(question_id: int, response: Response):
+    try:
+        question = await Question.objects.get(id = question_id)
+        return await question.delete()
+    except:
+        response.status_code = 404
+        return {'mensagem':'id nao encontrado'}
